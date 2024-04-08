0e8ee38e340792d9cb4fe3a028180700577050da
Diego Cuevas <diegocuevas579@gmail.com>
2024-04-07 22:28:06 -0500

    Add callbacks to delivery model

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -7,12 +7,37 @@ class Delivery < ApplicationRecord
     completed: 3,
     canceled: 4
   }
+
   validates :path_id, presence: true
   validates :description, presence: true, length: { maximum: 255 }
-  validates :status, presence: true
-  validates :delivery_date, presence: true
-  validates :delivery_time, presence: true
-  validates :signature, length: { maximum: 255 }
+  validates :signature, length: { maximum: 255 }, presence: true
   validates :observations, length: { maximum: 255 }
-  validates :delivery_photo, format: { with: /\Ahttps?:\/\/.*\z/, message: "must be a valid URL" }, allow_blank: true
+
+  before_save :build_sender_name
+  after_update :update_status_and_details, :build_receiver_name
+
+  private
+
+  def build_sender_name
+    if sender_first_name_changed? || sender_last_name_changed?
+      self.sender_full_name = [sender_first_name, sender_last_name].join(' ').strip
+    end
+  end
+
+  def build_receiver_name
+    if receiver_first_name_changed? || receiver_last_name_changed?
+      self.receiver_full_name = [receiver_first_name, receiver_last_name].join(' ').strip
+    end
+  end
+
+  def update_status_and_details
+    if receiver_signature_changed?
+      update_attributes(
+        status: :completed,
+        delivery_date: Time.zone.today,
+        delivery_time: Time.zone.now,
+      )
+    end
+  end
+
 end
